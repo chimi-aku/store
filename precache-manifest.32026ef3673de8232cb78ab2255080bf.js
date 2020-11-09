9727da2ad2c655b870c650fc81d4f759c55fc02e
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2020-11-09 09:58:36 +0100

    Updates

---
+++ precache-manifest.32026ef3673de8232cb78ab2255080bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfd068ef0710113faf75b0e71db54f0",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "af95fe2d58f077db3aa3",
+    "url": "/store/static/css/main.68b8a2e3.chunk.css"
+  },
+  {
+    "revision": "08c2b819ce5bf3c8d05c",
+    "url": "/store/static/js/2.f5bf5fd9.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/store/static/js/2.f5bf5fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af95fe2d58f077db3aa3",
+    "url": "/store/static/js/main.d18d535e.chunk.js"
+  },
+  {
+    "revision": "baa40557d47c32a07c05",
+    "url": "/store/static/js/runtime-main.2508a05c.js"
+  }
+]);
